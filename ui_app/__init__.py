4f95147edf184607463b239a6297b19dcd1dc7bd
Asov000 <166956370+Asov000@users.noreply.github.com>
2026-07-04 21:38:00 +0800

    Delete ui_app directory

--- ui_app/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""PyQt 标注界面包，导出主窗口和界面入口。"""
-
-from .annotator import SAMAnnotator
-from .main import run_app
-from .widgets import ImageLabel
-
-__all__ = ["ImageLabel", "SAMAnnotator", "run_app"]
